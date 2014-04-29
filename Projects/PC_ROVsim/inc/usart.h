3681afc5db1a7ad051459b0b03001873a96336cb
fahdovski <fahdus.united@gmail.com>
2014-04-29 18:29:23 +0100

    ROV C# Communication using Discovery F3

---
+++ Projects/PC_ROVsim/inc/usart.h
@@ -0,0 +1,8 @@
+#ifndef USART_H
+#define USART_H
+
+
+void init_USART1();
+void USART_puts(USART_TypeDef* USARTx,__IO char *s);
+
+#endif
---
+++ Projects/PC_ROVsim/src/main.c
@@ -0,0 +1,167 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  MCD Application Team
+  * @version V4.0.0
+  * @date    21-January-2013
+  * @brief   Virtual Com Port Demo main file
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "hw_config.h"
+#include "usb_lib.h"
+#include "usb_desc.h"
+#include "usb_pwr.h"
+#include "usart.h"
+
+
+extern __IO uint8_t Receive_Buffer[64];
+extern __IO  uint32_t Receive_length ;
+extern __IO  uint32_t length ;
+uint8_t Send_Buffer[64];
+uint32_t packet_sent=1;
+uint32_t packet_receive=1;
+
+
+static __IO uint32_t TimingDelay;
+void Delay(__IO uint32_t nTime);
+void Led_init(void);
+
+/*******************************************************************************
+* Function Name  : main.
+* Descriptioan    : Main routine.
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+int main(void)
+{
+  Set_System();
+  Set_USBClock();
+  USB_Interrupts_Config();
+  USB_Init();
+  init_USART1();
+  Led_init();
+  //Configure Systick timing
+ if (SysTick_Config(SystemCoreClock / 1000))
+  { 
+    /* Capture error */ 
+    STM_EVAL_LEDOn(LED10);
+    while (1);
+  }
+ 
+ 
+  
+  while (1)
+  {
+   // if (bDeviceState == CONFIGURED)
+  //  {
+     /* CDC_Receive_DATA();
+      //Check to see if we have data yet 
+      if (Receive_length  != 0)
+      {
+        if (packet_sent == 1)
+          CDC_Send_DATA ((unsigned char*)Receive_Buffer,Receive_length);
+        Receive_length = 0;
+      }*/
+      /* Toggle LED3 */
+    STM_EVAL_LEDToggle(LED3);  
+    /* Insert 100 ms delay */
+     USART_puts( USART1, "Hello daft punk rock ! beyond \n best tube \t ever \n");
+      Delay(500);
+  //  }
+  } 
+}
+
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in milliseconds.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{ 
+  TimingDelay = nTime;
+
+  while(TimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (TimingDelay != 0x00)
+  { 
+    TimingDelay--;
+  }
+}
+
+void Led_init(void)
+{
+   /* Initialize Leds mounted on STM32F3-discovery */
+  STM_EVAL_LEDInit(LED3);
+  STM_EVAL_LEDInit(LED4);
+  STM_EVAL_LEDInit(LED5);
+  STM_EVAL_LEDInit(LED6);
+  STM_EVAL_LEDInit(LED7);
+  STM_EVAL_LEDInit(LED8);
+  STM_EVAL_LEDInit(LED9);
+  STM_EVAL_LEDInit(LED10);
+  
+
+  STM_EVAL_LEDOn(LED3);
+  STM_EVAL_LEDOff(LED4);
+  STM_EVAL_LEDOff(LED5);
+  STM_EVAL_LEDOff(LED6);
+  STM_EVAL_LEDOff(LED7);
+  STM_EVAL_LEDOff(LED8);
+  STM_EVAL_LEDOff(LED9);
+  STM_EVAL_LEDOff(LED10);
+  
+
+}
+
+#ifdef USE_FULL_ASSERT
+/*******************************************************************************
+* Function Name  : assert_failed
+* Description    : Reports the name of the source file and the source line number
+*                  where the assert_param error has occurred.
+* Input          : - file: pointer to the source file name
+*                  - line: assert_param error line source number
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {}
+}
+#endif
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
